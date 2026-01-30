e7373eac53e7ef95f12a5bef6eb1d46fe8e242be
Withered Flowers <withered-flowers@users.noreply.github.com>
2026-01-30 10:16:56 +0700

    fix(config): Enable SSR with Node adapter to support dynamic routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
